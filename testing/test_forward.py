b8cb84be48b1c59d187ae10773184052c53b547f
lganic <55366941+lganic@users.noreply.github.com>
2026-04-23 13:20:45 -0400

    Fix bad variable name

--- testing/test_forward.py
+++ testing/test_forward.py
@@ -7,7 +7,7 @@ N = 100
 
 param = ParameterVector(N, starting_time = 30, starting_throttle=1, starting_angle=math.pi)
 
-initial_state = StateVector(0, 0, 500, 0)
+initial_state = StateVector(0, 0, -500, 0)
 
 Thrust = 500000
 Mass = 45000
@@ -27,15 +27,15 @@ states = sim(
 # Extract data
 x = [s.x for s in states]
 y = [s.y for s in states]
-vx = [thrust * math.cos(angle) for angle, thrust in zip(param.theta_n, param.tau_n)] + [0]
-vy = [thrust * math.sin(angle) for angle, thrust in zip(param.theta_n, param.tau_n)] + [0]
+arrow_x = [thrust * math.cos(angle) for angle, thrust in zip(param.theta_n, param.tau_n)] + [0]
+arrow_y = [thrust * math.sin(angle) for angle, thrust in zip(param.theta_n, param.tau_n)] + [0]
 
 # Plot trajectory
 plt.figure(figsize=(8, 6))
 plt.plot(x, y, label="Trajectory", linewidth=2)
 
 # Quiver 
-plt.quiver(x, y, vx, vy, angles='xy', scale_units='xy', scale=2e-3)
+plt.quiver(x, y, arrow_x, arrow_y, angles='xy', scale_units='xy', scale=2e-3)
 
 # Labels / formatting
 plt.xlabel("x")
