22063c7a7f202fbdda1a1e29d8dd1142ab86a277
lganic <55366941+lganic@users.noreply.github.com>
2026-04-23 08:26:54 -0400

    Add force implementation

--- testing/forces.py
+++ testing/forces.py
@@ -3,10 +3,25 @@ This file defines the forces which apply to the spacecraft during travel, mostly
 '''
 
 import math
+from vectors import StateVector
 
 R = (100000 / 3) * (1 + math.sqrt(10))
 g = 10
 
-def field(altitude, velocity):
-    pass
+def field(sv: StateVector):
     
+    # Simulate the force due to gravity in the Stormworks Physics model. 
+
+    ground_velocity = sv.vx
+
+    y = sv.y
+
+    return (0, 1 + (ground_velocity / 100) - (g * R * R) / math.pow(y + R, 2))
+
+def drag(sv: StateVector):
+
+    velocity = math.sqrt(math.pow(sv.vx, 2) + math.pow(sv.vy, 2))
+
+    accel_mag = -max(0, (velocity - 500) / 1.5)
+
+    return (accel_mag * sv.vx / velocity, accel_mag * sv.vy / velocity)
